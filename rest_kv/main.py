0bffa1d7b54cf83e977c21c5e08b9684717671bf
Prakash Yogi <prakash.y@fyle.in>
2023-05-01 01:33:42 +0530

    Added set key and delete key functionality

--- rest_kv/main.py
+++ rest_kv/main.py
@@ -16,5 +16,42 @@ def get_keys():
     return Response(value, status=200)
 
 
+@app.route("/set", methods=["POST"])
+def set_keys():
+    try:
+        data = request.get_json()
+        key = data.get("key")
+        value = data.get("value")
+
+        if not key or not value:
+            return Response("No key or value provided", status=400)
+
+        redis_client.set(key, value)
+        return Response(f"{key}:{value}")
+    except Exception as e:
+        print(e)
+        return Response(f"Server error occure: {e}", status=500)
+
+
+@app.route("/delete", methods=["DELETE"])
+def delete_keys():
+    try:
+        data = request.get_json()
+        key = data.get("key")
+
+        if not key:
+            return Response("No key provided", status=400)
+
+        status = redis_client.delete(key)
+        if not status:
+            return Response("No such key found", status=404)
+
+        return Response(f"{key} deleted")
+
+    except Exception as e:
+        print(e)
+        return Response(f"Server error occured: {e}", status=500)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
