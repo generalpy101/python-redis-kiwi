cf3942bec121ad3bed4f904b23f7fa6b5235da17
Prakash Yogi <prakash.y@fyle.in>
2023-05-01 00:38:00 +0530

    Added get functionality, removed mypy

--- rest_kv/main.py
+++ rest_kv/main.py
@@ -1,6 +1,20 @@
-def main_reader():
-    print("Hello World")
+import redis
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+
+@app.route("/get", methods=["GET"])
+def get_keys():
+    key = request.args.get("key")
+    if not key:
+        return Response("No key provided", status=400)
+    value = redis_client.get(key)
+    if not value:
+        return Response("No value found", status=404)
+    return Response(value, status=200)
 
 
 if __name__ == "__main__":
-    main_reader()
+    app.run(debug=True)
